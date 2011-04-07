d670c11690cc5eff17255c709f0a8ce725b24fd3
elenapechko <elenapechko@gmail.com>
2011-04-07 17:38:57 +0000

    Commit source code of the sample project

---
+++ TreeSample/test/collections/TraversalTestSuite.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package collections;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Pechko_E
+ *
+ */
+@RunWith(value=Suite.class)
+@SuiteClasses(value={TreeTest.class})
+public class TraversalTestSuite {
+
+}
