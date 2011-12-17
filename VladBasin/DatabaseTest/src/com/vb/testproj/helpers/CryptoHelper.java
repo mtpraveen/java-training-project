3bfde81d352b705a0764f67703bb99ae2cf43cda
vladbasin <vladbasin@gmail.com>
2011-12-17 20:56:54 +0000

    Added cryptography helper (cryptSHA method) in addition to standart crypt method (with MD5 algorithm).

--- VladBasin/DatabaseTest/src/com/vb/testproj/helpers/CryptoHelper.java
+++ VladBasin/DatabaseTest/src/com/vb/testproj/helpers/CryptoHelper.java
@@ -1,6 +1,5 @@
 package com.vb.testproj.helpers;
 
-
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -21,9 +20,23 @@ public class CryptoHelper {
 		return res.toString(16);
 
 	}
-	
-	public static boolean isMatch(String target, String cryptedSource) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+
+	public static boolean isMatch(String target, String cryptedSource)
+			throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		return crypt(target).equals(cryptedSource);
+	}
+
+	public static String cryptSha(String target) throws NoSuchAlgorithmException,
+			UnsupportedEncodingException {
+
+		MessageDigest md = MessageDigest.getInstance("SHA");
+
+		md.update(target.getBytes("UTF-16"));
+
+		BigInteger res = new BigInteger(1, md.digest(key.getBytes()));
+
+		return res.toString(16);
+
 	}
 
 }
