88b8307bbd2d7840351dce2867c2534b81d9c4ac
DmitrijDDD@gmail.com <DmitrijDDD@gmail.com@ce4aeede-771e-92c0-f6e2-8d4535f68bab>
2011-12-17 17:50:18 +0000

    new class

---
+++ trunk/DmitryGoncharov/Blog/src/main/java/by/epam/blog/model/Comment.java
@@ -0,0 +1,35 @@
+/**
+ * Trainig Sample project
+ * Admin
+ */
+package by.epam.blog.model;
+
+import java.util.Date;
+
+/**
+ * @author Admin
+ *
+ */
+public class Comment {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	private String text;
+	private Date date;
+}
