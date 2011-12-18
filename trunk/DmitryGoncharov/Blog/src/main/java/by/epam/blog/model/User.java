498ff362992186ecb5ae708fa9f6dc6603e5cdf8
DmitrijDDD@gmail.com <DmitrijDDD@gmail.com@ce4aeede-771e-92c0-f6e2-8d4535f68bab>
2011-12-18 14:21:21 +0000

    User.java added to database...

--- trunk/DmitryGoncharov/Blog/src/main/java/by/epam/blog/model/User.java
+++ trunk/DmitryGoncharov/Blog/src/main/java/by/epam/blog/model/User.java
@@ -1,18 +1,34 @@
-/**
- * Trainig Sample project
- * Admin
- */
 package by.epam.blog.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * @author Dmitry_Goncharov
  *
  */
-public class User {
-	private String login;
-	private String pass;
-	private String name;
+@Entity
+@Table(name = "USER")
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	@Column(name = "USER_LOGIN")
+	private String login;
+	@Column(name = "USER_PASS")
+	private String pass;
+	@Column(name = "USER_NAME")
+	private String name;
+	
 	public String getLogin() {
 		return login;
 	}
