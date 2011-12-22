db8aa8908ea177cec381e35b6fcb12543a5f3174
DmitrijDDD@gmail.com <DmitrijDDD@gmail.com@ce4aeede-771e-92c0-f6e2-8d4535f68bab>
2011-12-22 12:01:50 +0000

    temp

--- trunk/DmitryGoncharov/Blog/src/main/java/by/epam/blog/model/Blog.java
+++ trunk/DmitryGoncharov/Blog/src/main/java/by/epam/blog/model/Blog.java
@@ -1,15 +1,33 @@
 package by.epam.blog.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
 /**
  * @author Dmitry_Goncharov
  *
  */
-public class Blog {
+@Entity
+@Table(name = "BLOG")
+public class Blog implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	@Column(name = "BLOG_AUTHOR")
 	private User author;
+	
 	private List<Topic> topics = new ArrayList<Topic>();
 	
 	public long getId() {
