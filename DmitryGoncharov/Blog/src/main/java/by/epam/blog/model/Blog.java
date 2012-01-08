d5e747cb5c201a8755170600abfa7aa7073d5149
DmitrijDDD <DmitrijDDD@gmail.com>
2012-01-08 23:48:54 +0000

    fix

--- DmitryGoncharov/Blog/src/main/java/by/epam/blog/model/Blog.java
+++ DmitryGoncharov/Blog/src/main/java/by/epam/blog/model/Blog.java
@@ -66,43 +66,4 @@ public class Blog implements Serializable{
 	public void setUser(User user) {
 		this.user = user;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((topics == null) ? 0 : topics.hashCode());
-		result = prime * result + ((user == null) ? 0 : user.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Blog other = (Blog) obj;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (topics == null) {
-			if (other.topics != null)
-				return false;
-		} else if (!topics.equals(other.topics))
-			return false;
-		if (user == null) {
-			if (other.user != null)
-				return false;
-		} else if (!user.equals(other.user))
-			return false;
-		return true;
-	}
-	
 }
