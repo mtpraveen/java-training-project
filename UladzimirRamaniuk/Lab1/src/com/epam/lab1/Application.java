c70edb526e64f5e258f93edb8a473e3107c8ef62
vlavlarom <vlavlarom@gmail.com>
2012-10-18 14:54:52 +0000

    1,2 and 5 tasks for 1-st lab. This project include solution for these tasks. Class Matrix is used for sorting, transpose, shift and other operations with matrix.

---
+++ UladzimirRamaniuk/Lab1/src/com/epam/lab1/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.epam.lab1;
+
+/**
+ * @author Vladimir
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Labs labs = new Labs();
+		labs.Task5TranspMatrix();			
+	}
+
+}
