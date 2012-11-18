45ddab9cfe90ff2e9d98179438a16e85207f4329
Vitali.Hardzeyenka@gmail.com <Vitali.Hardzeyenka@gmail.com@ce4aeede-771e-92c0-f6e2-8d4535f68bab>
2012-11-18 16:45:50 +0000

    Add action in ConsoleManager, modified ClientManager.

---
+++ trunk/tasks/VitaliHardzeyenka/Hotel/HotelClient/src/com/epam/training/logic/Connecter.java
@@ -0,0 +1,29 @@
+package com.epam.training.logic;
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Connecter {
+
+	private ClientManager clientManager = new ClientManager();
+	private Logger logger = new Logger(org.apache.log4j.Logger.getLogger(Runner.class.getName()));
+	private String serverIp = "127.0.0.1";
+	private int serverPort = 1234;
+	private Socket serverSocket;
+
+	public void run() {
+		System.out.println("client was started");
+		try {
+			InetAddress address = InetAddress.getByName(serverIp); // get server address
+			serverSocket = clientManager.setServerSonnection(address, serverPort, logger.getExeptionsLogger()); // try to connect to server
+			System.out.println("client connected");
+			
+			// Give control to user.
+			ConsoleManager consoleManager = new ConsoleManager(serverSocket);
+			consoleManager.run();
+		} catch (UnknownHostException exception) {
+			logger.getExeptionsLogger().error(exception);
+		}
+	}
+}
--- trunk/tasks/VitaliHardzeyenka/Hotel/HotelClient/src/com/epam/training/logic/Runner.java
+++ trunk/tasks/VitaliHardzeyenka/Hotel/HotelClient/src/com/epam/training/logic/Runner.java
@@ -1,9 +1,6 @@
 package com.epam.training.logic;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Runner {
 
@@ -12,20 +9,9 @@ public class Runner {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-
-//		ClientManager clientManager = new ClientManager();
-//		Logger logger = new Logger(org.apache.log4j.Logger.getLogger(Runner.class));
-//		
-//		Socket serverSocket = null;
-//		try {
-//			serverSocket = clientManager.setServerSonnection(InetAddress.getLocalHost(), 1234, logger.getExeptionsLogger());
-//		} catch (UnknownHostException e1) { }
-//		
-//		try {
-//			serverSocket.close();
-//		} catch (IOException e) {
-//			// error
-//		}		
+		
+		Connecter connecter = new Connecter();
+		connecter.run();
 		
 	}
 
