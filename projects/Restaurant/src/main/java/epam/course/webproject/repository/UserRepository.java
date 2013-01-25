0c3ebe0cee9e0e87e1d8848b3f60900ac32be62f
kisel.nikolay <kisel.nikolay@gmail.com>
2013-01-25 12:34:49 +0000

    Delete project

--- projects/Restaurant/src/main/java/epam/course/webproject/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package epam.course.webproject.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import epam.course.webproject.domain.User;
-
-public interface UserRepository extends CrudRepository<User, String>{
-
-}
